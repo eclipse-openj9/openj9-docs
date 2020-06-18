f7d71a7c36d166cea2dc3148164cdeb3ca4907a9
genie-openj9 <openj9-bot@eclipse.org>
2020-06-18 10:11:55 +0000

    Generated from commit: 0f37fccd17433ba32b544e52c0bb2c4b56a11c95
    Signed-off-by: genie-openj9 <openj9-bot@eclipse.org>

---
+++ javascripts/oj9.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    if (document.getElementById("api") !== null) {
+        document.getElementsByClassName('md-content')[0].style.marginRight="auto";
+    } else {
+        document.getElementsByClassName('md-content')[0].style.marginRight="12.1rem";
+    }
+}
