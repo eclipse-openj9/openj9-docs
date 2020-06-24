3b140d5d7a5b1c8b69597bd9599ec5550f9b6b15
Bazif Khan <bazif.khan@ibm.com>
2020-06-24 11:49:13 +0100

    API docs mobile view fix
    Signed-off-by: Bazif Khan <bazif.khan@ibm.com>

--- docs/javascripts/oj9.js
+++ docs/javascripts/oj9.js
@@ -1,7 +1,5 @@
 window.onload = function () {
     if (document.getElementById("api") !== null) {
         document.getElementsByClassName('md-content')[0].style.marginRight="auto";
-    } else {
-        document.getElementsByClassName('md-content')[0].style.marginRight="12.1rem";
     }
 }
